842ca236143bb29db0a03809de4f67f761b29093
Przemek Pisanski <ppisanski@ventcode.io>
2017-06-03 20:36:00 +0200

    asddsa

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
-RAILS_ENV=production bundle exec rake assets:precompile
+  # RAILS_ENV=production bundle exec rake assets:precompile
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
