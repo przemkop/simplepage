635b33eb17ef839e1acfcbdf1bd07c539a1371dd
Przemek Pisanski <ppisanski@ventcode.io>
2017-06-03 19:18:49 +0200

    production set up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
-
+RAILS_ENV=production bundle exec rake assets:precompile
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
@@ -83,4 +83,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.assets.compile = true
 end
